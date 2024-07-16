2742039f0f0e8acf28882083fc39c26ead1ce563
Fionn Fitzmaurice <fionn@github.com>
2024-07-16 20:21:30 +0800

    Split treesitter, cmp, lsp configuration from plugins

---
+++ nvim/.config/nvim/lua/cmp_lsp.lua
@@ -0,0 +1,201 @@
+local cmp = require("cmp")
+local cmp_lsp = require("cmp_nvim_lsp")
+
+local kind_icons = {
+    Method = "𝑓",
+    Function = "𝑓",
+    Variable = "𝑥",
+    Snippet = "✂",
+    Constant = "𝜋",
+    TypeParameter = "𝑻",
+    Interface = "ɪ",
+    Module = "▭"
+}
+
+local function has_words_before()
+    -- From https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings.
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:
+        sub(col, col):match("%s") == nil
+end
+
+---@param text string
+---@param max_length integer
+---@return string
+local function truncate(text, max_length)
+    if text and text:len() > max_length then
+        text = text:sub(1, max_length) .. "⋯"
+    end
+    return text
+end
+
+cmp.setup {
+    enabled = function()
+        -- disable completion in comments
+        local context = require("cmp.config.context")
+        if vim.api.nvim_get_mode().mode == "c" then
+            return true
+        else
+            return not context.in_treesitter_capture("comment")
+                and not context.in_syntax_group("Comment")
+        end
+    end,
+
+    preselect = cmp.PreselectMode.None,
+
+    completion = {
+        autocomplete = false
+    },
+
+    formatting = {
+        format = function(_, vim_item)
+            vim_item.abbr = truncate(vim_item.abbr, 30)
+            vim_item.menu = truncate(vim_item.menu, 30)
+            vim_item.info = truncate(vim_item.info, 30)
+
+            vim_item.kind = kind_icons[vim_item.kind] or vim_item.kind
+
+            vim_item.dup = 0
+
+            return vim_item
+        end
+    },
+
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
+    },
+
+    experimental = {
+        ghost_text = true
+    },
+
+    mapping = cmp.mapping.preset.insert {
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<Esc>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.mapping.confirm({select = false})
+            end
+            cmp.abort()
+            fallback()
+        end, {"i", "s"}),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                if #cmp.get_entries() == 1 then
+                    cmp.confirm({ select = true })
+                elseif cmp.get_selected_entry() then
+                    cmp.select_next_item()
+                elseif not cmp.complete_common_string() then
+                    cmp.select_next_item()
+                end
+            elseif has_words_before() then
+                cmp.complete({reason = "manual"})
+                if #cmp.get_entries() == 1 then
+                    cmp.confirm({ select = true })
+                end
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, {"i", "s"})
+    },
+
+    sorting = {
+        comparators = {
+            cmp.config.compare.exact,
+            cmp.config.compare.sort_text
+        }
+    },
+
+    matching = {
+        disallow_fuzzy_matching = true,
+        disallow_fullfuzzy_matching = true,
+        disallow_partial_fuzzy_matching = true,
+        disallow_partial_matching = true,
+        disallow_prefix_unmatching = true
+    },
+
+    sources = cmp.config.sources {
+        {name = "nvim_lsp"},
+        {name = "nvim_lsp_signature_help"},
+        {name = "buffer"}
+    }
+}
+
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local lspconfig = require("lspconfig")
+require("lspconfig.ui.windows").default_options.border = "rounded"
+
+lspconfig.clangd.setup {
+    capabilities = cmp_lsp.default_capabilities()
+}
+
+lspconfig.rust_analyzer.setup {
+    capabilities = cmp_lsp.default_capabilities()
+}
+
+lspconfig.pylsp.setup {
+    -- TODO: configure this.
+    -- https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+    capabilities = cmp_lsp.default_capabilities(),
+    settings = {
+        pylsp = {
+            plugins = {
+                ruff = {
+                    ignore = {"E741"}
+                },
+                pylsp_mypy = {
+                    -- Once https://github.com/python-lsp/pylsp-mypy/pull/83
+                    -- makes it mainstream, we can drop this override.
+                    overrides = {true, "--no-pretty"}
+                }
+            }
+        }
+    }
+}
+
+lspconfig.lua_ls.setup {
+    capabilities = cmp_lsp.default_capabilities(),
+    settings = {
+        Lua = {
+            completion = {
+                keywordSnippet = "Disable"
+            },
+            diagnostics = {
+                globals = {"vim"}
+            }
+        }
+    }
+}
+
+lspconfig.texlab.setup {
+    -- TODO: https://github.com/latex-lsp/texlab/wiki/Configuration
+    capabilities = cmp_lsp.default_capabilities(),
+    settings = {
+        texlab = {
+            chktex = {
+                onEdit = true,
+                onOpenAndSave = true
+            },
+            completion = {
+                matcher = "prefix"
+            }
+        }
+    }
+}
+
+lspconfig.bashls.setup {}
+lspconfig.terraformls.setup {}
+lspconfig.tflint.setup {}
+lspconfig.gopls.setup {}
+lspconfig.marksman.setup {}
+lspconfig.yamlls.setup {}
---
+++ nvim/.config/nvim/lua/treesitter.lua
@@ -0,0 +1,48 @@
+require("nvim-treesitter.configs").setup {
+    ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "diff"},
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        disable = {
+            "gitcommit",
+            "markdown"
+        },
+        additional_vim_regex_highlighting = false
+    },
+    indent = {
+        enable = true,
+        disable = {
+            "python"
+        }
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            -- TODO: improve keybindings.
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm"
+        }
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = {query = "@class.inner", desc = "Select inner part of a class region"},
+                ["as"] = {query = "@scope", query_group = "locals", desc = "Select language scope"}
+            },
+            selection_modes = {
+                ["@parameter.outer"] = "v", -- charwise
+                ["@function.outer"] = "V", -- linewise
+                ["@class.outer"] = "<C-v>" -- blockwise
+            },
+            include_surrounding_whitespace = true
+        }
+    }
+}
