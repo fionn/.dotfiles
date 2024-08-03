0eacbacaf9c7ce5c7f815f93737aa90edb18a7cc
Fionn Fitzmaurice <fionn@github.com>
2024-08-03 13:42:12 +0800

    Use smaller info sign

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -67,7 +67,7 @@ vim.diagnostic.config {
         text = {
             [vim.diagnostic.severity.ERROR] = "⨯",
             [vim.diagnostic.severity.WARN] = "⚠",
-            [vim.diagnostic.severity.INFO] = "ⓘ",
+            [vim.diagnostic.severity.INFO] = "ℹ",
             [vim.diagnostic.severity.HINT] = "☞"
         }
     },
