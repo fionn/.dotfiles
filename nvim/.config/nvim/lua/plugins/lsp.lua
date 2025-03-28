576401d0367da5861c8065ce4c36897ab5f21a1b
Fionn Fitzmaurice <fionn@github.com>
2025-03-28 20:16:35 +0800

    Update nvim-lspconfig server configuration URL

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,4 @@
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
 
