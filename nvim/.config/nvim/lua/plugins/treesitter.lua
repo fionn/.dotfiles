1568916d268cff28be99b48eeac1c106f4244e3e
Fionn Fitzmaurice <fionn@github.com>
2025-12-24 21:11:13 +0800

    Drop C tree-sitter parser as default

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "diff"},
+    ensure_installed = {"lua", "vim", "vimdoc", "query", "diff"},
     sync_install = false,
     auto_install = true,
     highlight = {
