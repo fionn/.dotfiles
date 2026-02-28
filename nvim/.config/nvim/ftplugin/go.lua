4118c2a9392604058d421b45522a6264fb9eca15
Fionn Fitzmaurice <fionn@github.com>
2026-02-28 15:27:53 +0800

    Add keybinding for adding struct tags
    Adding stuct tags is my least favourite thing about writing Go.
    
    There is a third party tool gomodifytags that can do this, as well as
    a go-tagger.nvim plugin that implements this in Lua.
    
    However, gopls v0.19 added this via a code action by importing
    gomodifytags. This adds two new actions: refactor.rewrite.addTags and
    refactor.rewrite.removeTags. When over an untagged struct member, gopls
    offers to add tags. However, when over a tagged struct member, gopls
    offers to both add and remove tags. This means we can't (trivially at
    least) implement a toggle, without first checking the available code
    actions and so on. As such, we don't bother and only implement tag
    addition.
    
    One caveat is that gopls's implementation only offers JSON ta.
    
    See: https://github.com/fatih/gomodifytags
    See: https://github.com/romus204/go-tagger.nvim
    See: https://go.dev/gopls/features/transformation

--- nvim/.config/nvim/ftplugin/go.lua
+++ nvim/.config/nvim/ftplugin/go.lua
@@ -1,3 +1,18 @@
+local function add_struct_tags()
+    vim.lsp.buf.code_action({
+        context = {
+            diagnostics = {},
+            only = {"refactor.rewrite"},
+        },
+        filter = function(action)
+            return action.kind == "refactor.rewrite.addTags"
+        end,
+        apply = true
+    })
+end
+
+vim.api.nvim_create_user_command("GoAddTags", add_struct_tags, {desc = "Add struct tags"})
+
 vim.opt_local.colorcolumn = {80, 100}
 vim.opt_local.formatoptions:append({
     r = true,
