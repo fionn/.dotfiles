fionn/.dotfiles

commit d0b06f10ecf67535dbe9eab70ac356e066daeb8a

Quick close unmodifiable git files
These are the commits we see when blaming with git-signs.

nvim/.config/nvim/init.lua
@@ -143,9 +143,11 @@ vim.api.nvim_create_autocmd("TermOpen", {
 vim.api.nvim_create_autocmd("FileType", {
     group = vim.api.nvim_create_augroup("quick_close", {clear = true}),
     desc = "Close with q",
-    pattern = {"help", "checkhealth", "qf", "netrw", "gitsigns-blame"},
+    pattern = {"help", "checkhealth", "qf", "netrw", "gitsigns-blame", "git"},
     callback = function(event)
-        vim.keymap.set("n", "q", vim.cmd.close, {buffer = event.buf, silent = true})
+        if not vim.opt.modifiable:get() then
+            vim.keymap.set("n", "q", vim.cmd.close, {buffer = event.buf, silent = true})
+        end
     end
 })
 

commit 815408335e54556144360139c23abdce4824b414

Fix quick-closing for quickfix buffers
For some reason, when called the quickfix buffer doesn't register as
non-modifiable, so the keybinding is unset. Behaviourally this is
a regression introduced in d0b06f10ecf67535dbe9eab70ac356e066daeb8a.

nvim/.config/nvim/init.lua
@@ -156,9 +156,9 @@ vim.api.nvim_create_autocmd("FileType", {
     group = vim.api.nvim_create_augroup("quick_close", {clear = true}),
     desc = "Close with q",
     pattern = {"help", "checkhealth", "qf", "netrw", "gitsigns-blame", "git"},
-    callback = function(event)
-        if not vim.opt.modifiable:get() then
-            vim.keymap.set("n", "q", vim.cmd.close, {buffer = event.buf, silent = true})
+    callback = function(args)
+        if not vim.opt.modifiable:get() or args.match == "qf" then
+            vim.keymap.set("n", "q", vim.cmd.close, {buffer = args.buf, silent = true})
         end
     end
 })