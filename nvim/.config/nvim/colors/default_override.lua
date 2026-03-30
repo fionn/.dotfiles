2b38152b85df1989331bf6e470334d07b5f26d65
Fionn Fitzmaurice <fionn@github.com>
2026-03-30 23:41:53 +0800

    Use nvim_ui_send instead of io.write where possible
    It should be possible to use vim.api.nvim_ui_send in place of both calls
    to io.write, but the latter OSC 11 sequence fails to be picked up when
    sent that way.
    
    Instead, we replace the remaining io.write call with io.stdout:write, as
    it seems the default output may be changed in future.
    
    See: https://github.com/neovim/neovim/issues/16572#issuecomment-3574662113
    See: https://github.com/neovim/neovim/issues/10652#issuecomment-3218451632

--- nvim/.config/nvim/colors/default_override.lua
+++ nvim/.config/nvim/colors/default_override.lua
@@ -7,12 +7,12 @@ vim.api.nvim_create_autocmd({"UIEnter", "ColorScheme"}, {
     callback = function()
         local normal = vim.api.nvim_get_hl(0, {name = "Normal"})
         if normal.bg then
-            io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+            vim.api.nvim_ui_send(string.format("\027]11;#%06x\027\\", normal.bg))
             vim.api.nvim_create_autocmd("UILeave", {
                 group = "osc11",
                 desc = "Restore terminal background",
                 callback = function()
-                    io.write("\027]111\027\\")
+                    io.stdout:write("\027]111\027\\")
                 end
             })
         end
