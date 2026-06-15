689910c1d5291f88d25fbc8301945921686c495c
Fionn Fitzmaurice <fionn@github.com>
2026-06-15 17:34:42 +0800

    Remove .kube/config filetype detection
    We get this for free now.

--- nvim/.config/nvim/filetype.lua
+++ nvim/.config/nvim/filetype.lua
@@ -3,7 +3,6 @@ vim.filetype.add {
         ["osquery.conf"] = "jsonc",
     },
     pattern = {
-        ["${HOME}/.kube/config"] = "yaml",
         ["${XDG_STATE_HOME}/zsh/history"] = "zsh",
         ["playbook.ya?ml"] = "yaml.ansible",
         ["roles/[%a_]+/[%a_]+/[%a_]+.ya?ml"] = "yaml.ansible"
