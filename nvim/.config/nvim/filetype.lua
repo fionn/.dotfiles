5eab44e1ee4c9468cc71a148a1141f9a325406c0
Fionn Fitzmaurice <fionn@github.com>
2026-06-15 17:37:10 +0800

    Escape dots in ansible.yaml filetype detection

--- nvim/.config/nvim/filetype.lua
+++ nvim/.config/nvim/filetype.lua
@@ -4,7 +4,7 @@ vim.filetype.add {
     },
     pattern = {
         ["${XDG_STATE_HOME}/zsh/history"] = "zsh",
-        ["playbook.ya?ml"] = "yaml.ansible",
-        ["roles/[%a_]+/[%a_]+/[%a_]+.ya?ml"] = "yaml.ansible"
+        ["playbook%.ya?ml"] = "yaml.ansible",
+        ["roles/[%a_]+/[%a_]+/[%a_]+%.ya?ml"] = "yaml.ansible"
     }
 }
