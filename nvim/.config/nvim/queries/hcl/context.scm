7a133d6f80522faaa12c13ea24dac7dd0b5398a7
Fionn Fitzmaurice <fionn@github.com>
2026-06-23 22:41:28 +0800

    Add context query for HCL

---
+++ nvim/.config/nvim/queries/hcl/context.scm
@@ -0,0 +1,14 @@
+; Copied from Terraform context query.
+
+[
+  (block)
+  (object)
+  (object_elem)
+  (attribute)
+  (template_literal)
+] @context
+
+(for_object_expr
+  (_)
+  (for_intro) @context.start
+  (_) @context.end) @context
