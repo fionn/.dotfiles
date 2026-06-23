3c42fe197393eb53767dd6beac55148851b548c3
Fionn Fitzmaurice <fionn@github.com>
2026-06-23 23:59:36 +0800

    Revert "Comment C and C++ files with //"
    This is the default now.
    
    This reverts commit 64ceebad88f8118869e07d0def227a3f66e8c35c.

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -124,15 +124,6 @@ vim.api.nvim_create_autocmd("FileType", {
     end
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-    group = "options",
-    desc = "Comment C and C++ files with //",
-	pattern = {"c", "cpp"},
-	callback = function()
-		vim.opt_local.commentstring = "// %s"
-	end,
-})
-
 vim.api.nvim_create_autocmd("BufRead", {
     group = "options",
     desc = "Set read-only buffers as not modifiable",
