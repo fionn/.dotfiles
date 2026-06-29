5e52337e0f5397a8cf7808c61b77bb4221d9fc58
Fionn Fitzmaurice <fionn@github.com>
2026-06-29 23:09:39 +0800

    Continue quotes in markdown

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,9 @@
 vim.opt_local.formatoptions:remove("c")
 vim.opt_local.formatoptions:remove("t")
+vim.opt_local.formatoptions:append("r")
+
+vim.opt_local.comments:append("b:>")
+
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.winblend = 0
