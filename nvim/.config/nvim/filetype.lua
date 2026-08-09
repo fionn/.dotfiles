4a7ac3346b0e40bfbbdde1d2137c12aa8995dd3d
Fionn Fitzmaurice <git@fionn.computer>
2026-08-09 11:11:19 +0800

    Identify ~/.config/argocd/config as YAML

--- nvim/.config/nvim/filetype.lua
+++ nvim/.config/nvim/filetype.lua
@@ -4,6 +4,7 @@ vim.filetype.add {
     },
     pattern = {
         ["${XDG_STATE_HOME}/zsh/history"] = "zsh",
+        ["${XDG_CONFIG_HOME}/argocd/config"] = "yaml",
         ["playbook%.ya?ml"] = "yaml.ansible",
         ["roles/[%a_]+/[%a_]+/[%a_]+%.ya?ml"] = "yaml.ansible",
         ["${XDG_DATA_HOME}/nvim/site/spell/[%a-]+%.utf%-8%.add"] = "wordlist"
